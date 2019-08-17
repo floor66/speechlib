5bb95f777647454e98f7e727d208a4b578cc23ed
Floris <fpjdenhartog@gmail.com>
2019-08-17 11:24:46 +0200

    Using delta, not silence intervals

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import speech_recognition as sr
 import wave as wav
-from os import path
+from os import path, rename
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import FuncFormatter
 
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "samples/trump_1.wav")
-SILENCE_THRESHOLD = 1000
+OUT_FILE = "samples/out/full/phrase-{0}-{1}_{2}.wav"
+
+SILENCE_THRESHOLD = 500
+DELTA_THRESHOLD = 100
+WINDOW_SIZE = 10000
 MIN_SILENCE_LEN = 2000
-MIN_PHRASE_LEN = 2000
-DRAW = False
+MIN_PHRASE_LEN = 10000
+DRAW = True
 EXPORT = True
 RECOGNIZE = True
 
@@ -17,55 +22,61 @@ RECOGNIZE = True
 with wav.open(AUDIO_FILE, "rb") as src:
     signal = src.readframes(-1)
     signal = np.fromstring(signal, "Int16")
-
-    silences = []
-    silence_start = None
-    for i in range(len(signal)):
-        if abs(signal[i]) <= SILENCE_THRESHOLD: # there is silence
-            if silence_start is None: # we are not recording
-                    silence_start = i # start "recording" this silence
-        elif silence_start is not None: # there is sound, stop recording current silence?
-            if (i - silence_start) > MIN_SILENCE_LEN: # was this silence long enough?
-                silences.append((silence_start, i, i - silence_start))
-
-            silence_start = None
+    phrases = []
+    means = []
+    deltas = []
 
     if DRAW:
-        #plt.plot([pow(s, 2) for s in signal])
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
         #t = np.linspace(0, len(signal) / src.getframerate(), num=len(signal))
-        plt.plot(signal)
-        plt.axhline(SILENCE_THRESHOLD, color="black", linestyle="dashed")
-        plt.axhline(-SILENCE_THRESHOLD, color="black", linestyle="dashed")
+        ax.plot(abs(signal))
 
-    # phrase = end of prev silence - half of prev silence to start of next silence + half of next silence
-    phrases = []
-    prev_silence = (0, 0, 0) # start, end, length
-    for start, end, length in silences:
-        phrases.append((prev_silence[1] - (prev_silence[2] // 2), start + (length // 2)))
+    prev_wnd = None
+    phrase_start = None
+    j = 1
+    i = 0
+    while i < len(signal):
+        curr_wnd = abs(signal[i:i+WINDOW_SIZE])
 
-        #if DRAW:
-        #    plt.axvspan(start, end, color="black", alpha=0.25)
-        
-        prev_silence = (start, end, length)
+        if prev_wnd is not None:
+            delta = np.mean(curr_wnd) - np.mean(prev_wnd)
+
+            # High delta -> start of word
+            # Low delta (-) -> end of word
+            if delta > DELTA_THRESHOLD and phrase_start is None:
+                phrase_start = i-WINDOW_SIZE
+            elif delta > DELTA_THRESHOLD and phrase_start is not None and curr_wnd[-1] > SILENCE_THRESHOLD:
+                if i - phrase_start > MIN_PHRASE_LEN:
+                    phrases.append((phrase_start, i-WINDOW_SIZE))
+                    phrase_start = i-WINDOW_SIZE
+
+            deltas.append((i, i+WINDOW_SIZE, delta))
+            means.append((i, i+WINDOW_SIZE, np.mean(curr_wnd)))
+
+        prev_wnd = curr_wnd
+        i += WINDOW_SIZE
+        j += 1
+
+    if DRAW:
+        ax2 = ax.twinx()
+        ax2.plot([start + ((end - start) // 2) for start, end, _ in means], [mean for _, _, mean in means], color="red")
 
     print(len(phrases), "phrases")
     i = 0
     for start, end in phrases:
         i += 1
 
-        if (end - start) < MIN_PHRASE_LEN:
-            continue
-
         if DRAW:
-            plt.axvline(start, color="black")
-            plt.axvline(end, color="black")
-            plt.axvspan(start, end, color="red", alpha=0.25)
-            plt.text(start, 1, str(i))
+            ax.axvline(start, color="black")
+            ax.axvline(end, color="grey")
+            ax.axvspan(start, end, color="red", alpha=0.25)
+            ax.text(start, 1, str(i))
         
         if EXPORT:
-            OUT_FILE = "samples/out/full/%i/phrase-%i-%i_%i.wav" % (SILENCE_THRESHOLD, i, start, end)
+            _OUT_FILE = OUT_FILE.format(i, start, end)
 
-            with wav.open(OUT_FILE, "w") as out:
+            with wav.open(_OUT_FILE, "w") as out:
                 out.setnchannels(1)
                 out.setsampwidth(2)
                 out.setframerate(44100.0)
@@ -77,12 +88,14 @@ with wav.open(AUDIO_FILE, "rb") as src:
             if RECOGNIZE:
                 # use the audio file as the audio source
                 r = sr.Recognizer()
-                with sr.AudioFile(OUT_FILE) as source:
+                with sr.AudioFile(_OUT_FILE) as source:
                     audio = r.record(source)  # read the entire audio file
 
                 # # recognize speech using Google Speech Recognition
                 try:
-                    print("phrase_%i: %s" % (i, r.recognize_google(audio)))
+                    s = r.recognize_google(audio)
+                    print("phrase_%i: %s" % (i, s))
+                    rename(_OUT_FILE, _OUT_FILE.replace(".wav", "_%s.wav" % s))
                 except sr.UnknownValueError:
                     pass
                     #print("Google Speech Recognition could not understand audio")
@@ -94,6 +107,8 @@ with wav.open(AUDIO_FILE, "rb") as src:
                     #print(e)
 
     if DRAW:
+        ax.set_xlabel("Time (s)")
+        ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: "%.2f" % (float(x) / 44100.0)))
         plt.show()
 
 
